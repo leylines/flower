0ec24e341f0bb74b564976a8fc8be89b5f777393
Joerg Roth <info@leylines.ch>
2017-03-21 21:37:08 +0100

    major changes to grid

--- common.js
+++ common.js
@@ -111,7 +111,7 @@ function circleLayout(points, pointWidth, width, height) {
 
   var xOffset = width / 2;
   var yOffset = height / 2;
-  const periods = 200;
+  const periods = 40;
 
   const thetaScale = d3.scaleLinear()
     .domain([0, points.length - 1])
@@ -120,42 +120,29 @@ function circleLayout(points, pointWidth, width, height) {
   var sCount = 3;
   var sSize = 1000;
 
-/**
   var radius  = (height / 2) - 0.5 * pointWidth;
   for (i = 0; i < (sSize * sCount); i++) {
     points[i].x = radius * Math.cos(thetaScale(i)) + xOffset;
     points[i].y = radius * Math.sin(thetaScale(i)) + yOffset;
   }
 
-  var radius  = (height / 8 * 3.94) - 0.5 * pointWidth;
-  for (i = (sCount * sSize); i < (sSize * (sCount + 3)); i++) {
-    points[i].x = radius * Math.cos(thetaScale(i)) + xOffset;
-    points[i].y = radius * Math.sin(thetaScale(i)) + yOffset;
-  }
-  sCount = sCount + 3;
-*/
-
-  radius = (height / 8.0);
-  xOffset = radius;
-  yOffset = radius;
-  sinOffset = -1.0 * Math.sin(60) * radius;
-  cosOffset = -1.0 * Math.cos(60) * radius;
-
-console.log(radius);
-console.log(sinOffset);
-console.log(cosOffset);
+  var radius  = (height / 8) - 0.2 * pointWidth;
+  xOffset = radius / 2 + 3;
+  yOffset = radius / 2 + 5;
+  //sinOffset = -1.0 * Math.sin(60) * radius;
+  //cosOffset = -1.0 * Math.cos(60) * radius;
+  cosOffset = Math.sqrt(Math.pow(radius,2) - (Math.pow((radius /2),2)));
 
   var matrix = []
-  for (k=0; k<6; k++) { 
-    for (j=0; j<20; j++) { 
-       x = j * (cosOffset / 2);
-       y = k * (radius / 2);
+  for (k=1.0; k<8.0; k++) { 
+    for (j=1.0; j<17.0; j++) { 
+       x = j * (radius / 2.0);
+       y = k * (cosOffset);
        matrix.push([x,y])    
     }
   }
 
-  var flower = [0,2,4,6,8,41,43,45]
-  //var flower = [0,20,40];
+  var flower = [3,5,7,9,18,20,22,24,26,33,35,37,39,41,43,48,50,52,54,56,58,60,65,67,69,71,73,75,82,84,86,88,90,99,101,103,105];
   flower.map( function(m) {
     for (i = (sCount * sSize); i < ((sCount + 1) * sSize); i++) {
       points[i].x = radius * Math.cos(thetaScale(i)) + xOffset + matrix[m][0];
@@ -163,42 +150,7 @@ console.log(cosOffset);
     }
     sCount++;
   });
-/**
-  for (j=0; j<6; j++) { 
-    for (i = (sCount * sSize); i < ((sCount + 1) * sSize); i++) {
-      points[i].x = radius * Math.cos(thetaScale(i)) + xOffset + Math.cos(j * Math.PI / 3) * radius;
-      points[i].y = radius * Math.sin(thetaScale(i)) + yOffset + Math.sin(j * Math.PI / 3) * radius;
-    }
-    sCount++;
 
-    secondxOffset = xOffset + Math.cos(j * Math.PI / 3) * radius 
-    secondyOffset = xOffset + Math.sin(j * Math.PI / 3) * radius 
-
-    var x = [j, j+1].map( function(k) {
-      for (i = (sCount * sSize); i < ((sCount + 1) * sSize); i++) {
-        points[i].x = radius * Math.cos(thetaScale(i)) + secondxOffset +  Math.cos(k * Math.PI / 3) * radius;
-        points[i].y = radius * Math.sin(thetaScale(i)) + secondyOffset +  Math.sin(k * Math.PI / 3) * radius;
-      }
-      sCount++;
-      var t1 = [1, 3, 5]
-      var t = [];
-      if (t1.indexOf(k) >= 0) {
-        t = [k];
-      } else {
-        t = [k, k+1];
-      }
-      thirdxOffset = secondxOffset + Math.cos(k * Math.PI / 3) * radius 
-      thirdyOffset = secondyOffset + Math.sin(k * Math.PI / 3) * radius 
-      var x = [k, k+1].map( function(l) {
-        for (i = (sCount * sSize); i < ((sCount + 1) * sSize); i++) {
-          points[i].x = radius * Math.cos(thetaScale(i)) + thirdxOffset +  Math.cos(l * Math.PI / 3) * radius;
-          points[i].y = radius * Math.sin(thetaScale(i)) + thirdyOffset +  Math.sin(l * Math.PI / 3) * radius;
-        }
-        sCount++;
-      });
-    });
-  }
-*/
 
   console.log(i);
   console.log(sCount);
